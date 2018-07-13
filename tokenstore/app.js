a27555463908f4d6caecded96c2a4662376a3208
michivo <michivo@gmail.com>
2018-07-13 14:19:16 +0200

    add web api

--- tokenstore/app.js
+++ tokenstore/app.js
@@ -17,9 +17,13 @@
 
 // [START app]
 const express = require('express');
-const dbwrapper = require('./dbwrapper')
+const dbwrapper = require('./dbwrapper');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', (req, res) => {
   console.log('received request, sending response');
@@ -27,18 +31,23 @@ app.get('/', (req, res) => {
   res.status(200).send(result).end();
 });
 
-app.get('/add', (req, res) => {
-  console.log('received add request, sending response');
-  dbwrapper.sayHelloInGerman()
-    .then((count) => {
-      res.status(200).send('Hello, world! ' + count).end();
-  });
+app.get('/registerTokens', (req, res) => {
+  console.log('received request, sending response');
+  var result = '<html><head><title>Hello</title></head><body><form action="/registerTokens" method="post"><input type="hidden" name="tokens"' + ' value=\'[{ "index": 1, "tokenId": "t1234", "blindingFactor": "b123456" }, { "index": 2, "tokenId": "t4321", "blindingFactor": "b654321" }]\'/>' + '<input type="hidden" name="registrationId" value="1231231232"/><input type="hidden" name="votingId" value="1231231232"/><input type="hidden" name="email" value="michfasch@gmx.at"/><input type="submit" value="foo" /></form>';
+  res.status(200).send(result).end();
 });
 
-app.post('/foo', (req, res) => {
-  console.log('posted for foo');
-  res.status(200).send('hello foo').end();
-})
+app.post('/registerTokens', (req, res) => {
+  var registrationId = req.body.registrationId;
+  var votingId = req.body.votingId;
+  var email = req.body.email;
+  var tokens = JSON.parse(req.body.tokens);
+
+  dbwrapper.registerTokens(registrationId, votingId, email, tokens)
+    .then(() => {
+      res.status(200).send("OK!").end;
+  });
+});
 
 // Start the server
 const PORT = process.env.PORT || 8082;
