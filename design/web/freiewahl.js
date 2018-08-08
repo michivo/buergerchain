7491af9725231d8fbb4d2ddad95cb9bb74dd17d7
michivo <michivo@gmail.com>
2018-08-08 10:41:41 +0200

    start working on first design draft

---
+++ design/web/freiewahl.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $('#functions-slick').slick({
+    dots: true,
+    autoplay: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
