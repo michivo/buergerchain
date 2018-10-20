644863ce74294794eeb7eca997986e45e4a93cce
michivo <michivo@gmail.com>
2018-10-20 23:49:23 +0200

    send proper html mail

--- tokenstore/mailprovider.js
+++ tokenstore/mailprovider.js
@@ -8,7 +8,7 @@ function sendInvitation(email, votingTitle, startDate, endDate, link) {
     to: email,
     from: privateConfig.MAIL_FROM,
     subject: 'Erfolgreiche Registrierung für Abstimmung ' + votingTitle,
-    text: `LiebeR WahlberechtigteR,<br /> Ihre Registrierung für die Abstimmung <strong>${votingTitle}</strong> war erfolgreich.` +
+    html: `LiebeR WahlberechtigteR,<br /> Ihre Registrierung für die Abstimmung <strong>${votingTitle}</strong> war erfolgreich.` +
       `Sie können zwischen ${startDate} und ${endDate} unter <a href="${link}">${link}</a> an der Abstimmung teilnehmen.<br />` +
       'Wir freuen uns, wenn Sie von Ihrem Stimmrecht Gebrauch machen!'
   };
