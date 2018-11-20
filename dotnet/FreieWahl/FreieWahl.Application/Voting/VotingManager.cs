15f2dc58f1ddb4d0be34b0b6a973df7bee4718c1
michivo <michivo@gmail.com>
2018-11-20 23:40:13 +0100

    add logging

--- dotnet/FreieWahl/FreieWahl.Application/Voting/VotingManager.cs
+++ dotnet/FreieWahl/FreieWahl.Application/Voting/VotingManager.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FreieWahl.Common;
 using FreieWahl.Voting.Models;
 using FreieWahl.Voting.Storage;
+using Microsoft.Extensions.Logging;
 
 namespace FreieWahl.Application.Voting
 {
     public class VotingManager : IVotingManager
     {
-        private IVotingStore _votingStore;
+        private readonly IVotingStore _votingStore;
+        private readonly ILogger _logger;
 
         public VotingManager(IVotingStore votingStore)
         {
+            _logger = LogFactory.CreateLogger("Application.Registrations.BudgetSmsChallengeService");
             _votingStore = votingStore;
         }
 
@@ -30,6 +34,11 @@ namespace FreieWahl.Application.Voting
         public async Task<StandardVoting> GetById(string id)
         {
             var voting = await _votingStore.GetById(id);
+            if (voting == null)
+            {
+                _logger.LogWarning("Could not find voting with id " + id);
+                return null;
+            }
             await _UpdateVotingStates(voting);
 
             return voting;
@@ -39,6 +48,7 @@ namespace FreieWahl.Application.Voting
         {
             if (DateTime.UtcNow > voting.EndDate && voting.State != VotingState.Closed)
             {
+                _logger.LogInformation("Voting is over for voting with id " + voting.Id);
                 if (voting.Questions.Any(x => x.Status != QuestionStatus.Locked))
                 {
                     foreach (var question in voting.Questions)
@@ -74,6 +84,7 @@ namespace FreieWahl.Application.Voting
             var voting = await _votingStore.GetById(votingId).ConfigureAwait(false);
             if (voting.State == VotingState.Closed)
             {
+                _logger.LogError("Someone tried to modify a voting that has already been closed " + votingId);
                 throw new InvalidOperationException("This voting has already been closed and cannot be edited anymore");
             }
         }
@@ -104,6 +115,7 @@ namespace FreieWahl.Application.Voting
 
         public async Task Delete(string votingId)
         {
+            _logger.LogInformation("Deleting voting with id " + votingId);
             await _CheckVotingEditable(votingId).ConfigureAwait(false);
             await _votingStore.Delete(votingId).ConfigureAwait(false);
         }
